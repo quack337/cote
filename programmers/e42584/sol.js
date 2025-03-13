95f5b5eedddea6911c72e38b1c60f34bc036b73c
Lee, Seungjin <quack337@gmail.com>
2025-03-13 10:47:33 +0900

    ...

---
+++ programmers/e42584/sol.js
@@ -0,0 +1,20 @@
+function solution(prices) {
+  function removeLarger(stack, current) {
+      while (stack.length > 0) {
+          let e = stack[stack.length - 1];
+          if (e.price <= current.price) return;
+          answer[e.index] = current.index - e.index;
+          stack.pop();
+      }        
+  }
+  let answer = [], stack = [];
+  for (let i = 0; i < prices.length; ++i) {
+      let current = { price: prices[i], index: i };
+      removeLarger(stack, current)
+      stack.push(current);
+  }
+  removeLarger(stack, { price: 999999, index: prices.length - 1 })
+  return answer;
+}
+
+console.log(solution([1, 2, 3, 2, 3]));
