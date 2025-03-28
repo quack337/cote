e2b4e4c9cdb5cf9a5c895f9f4d8e470dbdd31ace
Lee, Seungjin <quack337@gmail.com>
2025-03-28 11:57:30 +0900

    ...

--- programmers/e42747/Main.java
+++ programmers/e42747/Main.java
@@ -4,9 +4,9 @@ public class Main {
     static class Solution {
         public int solution(int[] citations) {
             Arrays.sort(citations);
-            for (int i = 0; i < citations.length; ++i)
-                if (citations.length - i <= citations[i])
-                    return citations.length - i;
+            for (int h = citations.length; h > 0; --h)
+              if (citations[citations.length - h] >= h)
+                return h;
             return 0;
         }
     }
--- programmers/e42747/sol1.js
+++ programmers/e42747/sol1.js
@@ -1,15 +1,11 @@
 function solution(citations) {
-  for (let h = citations.length; h > 0; --h) {
-    let count = 0;
-    for (let value of citations)
-      if (value >= h)
-        ++count;
-    if (count >= h)
+  citations.sort((a, b) => a - b);
+  for (let h = citations.length; h > 0; --h)
+    if (citations[citations.length - h] >= h)
       return h;
-  }
   return 0;
 }
 
 console.log(solution([3, 0, 6, 1, 5]));
 console.log(solution([3, 3]));
-console.log(solution([0]));
+console.log(solution([]));
