f89c773190cc28063621924693d52536a4937629
Lee, Seungjin <quack337@gmail.com>
2025-03-31 10:02:39 +0900

    ...

--- Test.java
+++ Test.java
@@ -1,23 +1,40 @@
-import java.util.Arrays;
+package net.skhu.string_matching;
 
 public class Test {
+
     static int[] preprocessing(String P) {
-      int[] pi = new int[P.length() + 1];
-      int j = 0, k = -1;
-      pi[0] = -1;
-      while (j < P.length()) {
-          if (k == -1 || P.charAt(j) == P.charAt(k)) {
-              ++j; ++k;
-              pi[j] = k;
+        int[] pi = new int[P.length() + 1];
+        int j = 0, k = -1;
+        pi[0] = -1;
+        while (j < P.length()) {
+            if (k == -1 || P.charAt(j) == P.charAt(k)) {
+                ++j; ++k;
+                pi[j] = k;
+            } else
+                k = pi[k];
+        }
+        return pi;
+    }
+
+    // s 문자열에서 p 부분 문자열을 찾아서 인덱스를 리턴한다.
+    static int findIndex(String A, String P) {
+      int n = A.length(), m = P.length();
+      int[] pi = preprocessing(P);
+      int i = 0, j = 0;
+      while (i < n) {
+          if (j == -1 || A.charAt(i) == P.charAt(j)) {
+              ++i; ++j;
           } else
-              k = pi[k];
+              j = pi[j];
+          if (j == m)
+              return i - m; // 매칭 발견
       }
-      return pi;
+      return -1; // 일치하는 문자열을 찾지 못한 경우에 -1을 리턴
   }
 
-  public static void main(String[] args) {
-    String p = "ababc";
-    int[] pi = preprocessing(p);
-    System.out.println(Arrays.toString(pi));
-  }
+    public static void main(String[] args) {
+        String s = "abcabababcaababcdabcababcfabababcabab";
+        String p = "ababc";
+        System.out.println(findIndex(s, p));
+    }
 }
