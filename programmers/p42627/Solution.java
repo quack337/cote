9678724cf5c0c3af65e34ec35223f272056c52b0
Lee, Seungjin <quack337@gmail.com>
2025-04-26 11:43:59 +0900

    ...

--- programmers/p42627/Solution.java
+++ programmers/p42627/Solution.java
@@ -3,17 +3,18 @@ public class Solution {
     java.util.Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
     var queue = new java.util.PriorityQueue<int[]>((a, b) ->
       a[2] != b[2] ? a[2] - b[2] : a[1] != b[1] ? a[1] - b[1] : a[0] - b[0]);
-    int i = 0, s = 0, answer = 0;
+    int i = 0, end = -1, answer = 0;
     for (int t = 0; i < jobs.length || queue.size() > 0; ++t) {
       while (i < jobs.length && t == jobs[i][0]) {
         queue.add(new int[] {i, jobs[i][0], jobs[i][1]});
         ++i;
       }
-      if (t == s && queue.size() > 0) {
+      boolean working = (end != -1 && t < end);
+      if (!working && queue.size() > 0) {
         var job = queue.remove();
-        s = t + job[2];
-        answer += s - job[1];
-        //System.out.printf("%d [%d %d] %d\n", s, job[1], job[2], answer);  
+        end = t + job[2];
+        answer += end - job[1];
+        //System.out.printf(" %d [%d %d] %d\n", end, job[1], job[2], answer);  
       }
     }
     return answer / jobs.length;
@@ -22,5 +23,7 @@ public class Solution {
   public static void main(String[] args) {
     var sol = new Solution();
     System.out.println(sol.solution(new int[][] {{0,3},{1,9},{3,5}}));
+    System.out.println(sol.solution(new int[][] {{0,3},{1,9},{2,6}}));
+    System.out.println(sol.solution(new int[][] {{1,5},{10,20},{10,9},{10,10}}));
   }
 }
