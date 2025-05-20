7a6370b63af8851e1d3288f3213b1fb9eb8deefd
Lee, Seungjin <quack337@gmail.com>
2025-05-20 10:48:09 +0900

    ...

--- baekjoon/b14940/Main.java
+++ baekjoon/b14940/Main.java
@@ -1,40 +1,54 @@
-package baekjoon.b2178;
+package baekjoon.b14940;
 
 import java.io.*;
 import java.util.*;
 
 public class Main {
 
-  static int ROW, COL;
+  static int ROW, COL, r0, c0;
   static char[][] A;
+  static int[][] distances;
 
-  static int BFS() {
+  static void BFS() {
     var visited = new boolean[ROW][COL];
     var queue = new ArrayDeque<int[]>();
-    queue.add(new int[] {0, 0, 1});
+    queue.add(new int[] {r0, c0, 0});
     while (queue.size() > 0) {
       var u = queue.remove();
       int r = u[0], c = u[1], distance = u[2];
-      if (r == ROW - 1 && c == COL - 1) return distance;
       if (A[r][c] == '0') continue; // 벽
       if (visited[r][c]) continue;
       visited[r][c] = true;
+      distances[r][c] = distance;
       if (r > 0) queue.add(new int[] {r-1, c, distance+1});
       if (c > 0) queue.add(new int[] {r, c-1, distance+1});
       if (r < ROW-1) queue.add(new int[] {r+1, c, distance+1});
       if (c < COL-1) queue.add(new int[] {r, c+1, distance+1});
     }
-    return -1;
   }
 
   public static void main(String[] args) {
     var scanner = new Scanner(new BufferedInputStream(System.in));
     ROW = scanner.nextInt();
     COL = scanner.nextInt();
-    A = new char[ROW][];
+    A = new char[ROW][COL];
+    distances = new int[ROW][COL];
     for (int r = 0; r < ROW; ++r)
-      A[r] = scanner.next().toCharArray();
+      for (int c = 0; c < COL; ++c) {
+        A[r][c] = scanner.next().charAt(0);
+        if (A[r][c] == '2') {
+          r0 = r; c0 = c;
+        }
+      }
     scanner.close();
-    System.out.println(BFS());
+    BFS();
+    var builder = new StringBuilder();
+    for (int r = 0; r < ROW; ++r)
+      for (int c = 0; c < COL; ++c) {
+        int distance = distances[r][c];
+        if (distance == 0 && A[r][c] == '1') distance = -1;
+        builder.append(distance).append(c < COL-1 ? ' ' : '\n');
+      }
+    System.out.println(builder.toString());
   }
 }
