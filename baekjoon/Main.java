56b2600deca468fe0f0501ffdb55bebe015d884a
Lee, Seungjin <quack337@gmail.com>
2025-05-22 17:15:07 +0900

    b16928

--- baekjoon/Main.java
+++ baekjoon/Main.java
@@ -1,35 +1,15 @@
 package baekjoon;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    @SuppressWarnings("unused")
-    public static void main(String[] args) throws IOException {
-        var reader = new BufferedReader(new InputStreamReader(System.in));
-        var tokenizer = new StringTokenizer(reader.readLine());
-        int N = Integer.parseInt(tokenizer.nextToken());
-        int M = Integer.parseInt(tokenizer.nextToken());
-        int[] A = new int[N];
-        tokenizer = new StringTokenizer(reader.readLine());
-        for (int i = 0; i < N; ++i)
-            A[i] = Integer.parseInt(tokenizer.nextToken());
-
-        var builder = new StringBuilder();
-        for (int i = 0; i < N; ++i) {
-            builder.append(A[i]);
-            builder.append(' ');
-        }
-        System.out.println(builder.toString());
-        /*
-        var writer = new BufferedWriter(new OutputStreamWriter(System.out));
-        for (int i = 0; i < N; ++i) {
-            writer.write(String.valueOf(A[i]));
-            writer.write(' ');
-        }
-        writer.flush();
-        */
-    }
+  public static void main(String[] args) throws IOException {
+    var scanner = new Scanner(new BufferedInputStream(System.in));
+    int N = scanner.nextInt();;
+    int[] A = new int[N];
+    for (int i = 0; i < N; ++i)
+      A[i] = scanner.nextInt();
+    scanner.close();
+  }
 }
