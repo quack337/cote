45fdb73ddc0c3e0b1b69422335bb6c9d262fe0ec
Lee, Seungjin <quack337@gmail.com>
2025-05-23 14:53:05 +0900

    b16954

--- Main.java
+++ Main.java
@@ -1,19 +1,39 @@
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
 
 public class Main {
+  static int ROW, COL;
+  static char[][] A;
+
+  static int BFS() {
+    var visited = new boolean[ROW][COL];
+    var queue = new ArrayDeque<int[]>();
+    queue.add(new int[] {0, 0, 0});
+    while (queue.size() > 0) {
+      int[] u = queue.remove();
+      int r = u[0], c = u[1], distance = u[2];
+      if (A[r][c] == '#') continue;
+      if (visited[r][c]) continue;
+      visited[r][c] = true;
+      if (r == ROW-1 && c == COL-1) return distance;
+      if (r > 0) queue.add(new int[] {r-1, c, distance+1});
+      if (c > 0) queue.add(new int[] {r, c-1, distance+1});
+      if (r < ROW-1) queue.add(new int[] {r+1, c, distance+1});
+      if (c < COL-1) queue.add(new int[] {r, c+1, distance+1});
+    }
+    return -1;
+  }
 
   public static void main(String[] args) {
-    int[][] A = new int[][] {{4, 2}, {3, 5}, {1, 2}, {7, 5}, {4, 3}, {2, 2}};
-    Comparator<int[]> comparator = (a, b) -> {
-      if (a[0] != b[0]) return a[0] - b[0];
-      return a[1] - b[1];
-    };
-    Arrays.sort(A, comparator);
-    int i = Arrays.binarySearch(A, new int[] {4, 3}, comparator);
-    System.out.println(i);
-
-    int j = Arrays.binarySearch(A, new int[] {4, 4}, comparator);
-    System.out.printf("%d, %d\n", j, -j - 1);
+    ROW = 5; COL = 9;
+    String[] input = {
+      ".#...#...",
+      ".#.#...#.",
+      "...#.#.#.",
+      "##.#.#.#.",
+      ".....#..."};
+    A = new char[ROW][];
+    for (int r = 0; r < ROW; ++r)
+      A[r] = input[r].toCharArray();
+    System.out.println(BFS());
   }
 }
