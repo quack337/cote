b7c89ea8472eacf8dd805d4193463bac22a527b5
Lee, Seungjin <quack337@gmail.com>
2025-05-24 13:08:00 +0900

    b2805

--- baekjoon/b2805/sol.js
+++ baekjoon/b2805/sol.js
@@ -2,17 +2,19 @@ let fs = require('fs');
 let input = fs.readFileSync(0).toString().split(/[ \n\r]+/);
 let N = parseInt(input[0]);
 let M = parseInt(input[1]);
-let A = [];
-for (let i = 0; i < N; ++i)
+let A = [], max = 0
+for (let i = 0; i < N; ++i) {
   A[i] = parseInt(input[i + 2]);
+  if (A[i] > max) max = A[i];
+}
 
-console.log(파라매트릭서치_최대값(0, 1_000_000_000));
+console.log(파라매트릭서치_최대값(0, max));
 
-function compare(height) {
+function compare(middle) {
   let sum = 0;
   for (let tree of A)
-    if (tree > height)
-      sum += tree - height;
+    if (tree > middle)
+      sum += tree - middle;
   return M - sum;
 }
 
