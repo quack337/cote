bce6afb44a1f7eab1989a371c0f5666d7a1ecf43
Lee, Seungjin <quack337@gmail.com>
2025-05-29 10:50:43 +0900

    b6497

---
+++ baekjoon/b6497/Main.java
@@ -0,0 +1,50 @@
+package baekjoon.b6497;
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  static int V, E;
+  static ArrayList<int[]>[] links;
+
+  static int prim(int start) {
+    int costSum = 0;
+    var visited = new boolean[V+1];
+    var queue = new PriorityQueue<int[]>((a, b) -> a[1] - b[1]);
+    queue.add(new int[] {start, 0});
+    while (queue.size() > 0) {
+      int[] u = queue.remove();
+      int node = u[0], cost = u[1];
+      if (visited[node]) continue;
+      visited[node] = true;
+      costSum += cost;
+      for (int[] link : links[node])
+        queue.add(link);
+    }
+    return costSum;
+  }
+
+  @SuppressWarnings("unchecked")
+  public static void main(String[] args) throws IOException {
+    var scanner = new Scanner(new BufferedInputStream(System.in));
+    while (true) {
+      V = scanner.nextInt();
+      E = scanner.nextInt();
+      if (V == 0) break;
+      links = new ArrayList[V];
+      for (int i = 0; i < V; ++i)
+        links[i] = new ArrayList<>();
+      int minCost = Integer.MAX_VALUE, minNode = 0, costSum = 0;
+      for (int i = 0; i < E; ++i) {
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int cost = scanner.nextInt();
+        links[a].add(new int[] {b, cost});
+        links[b].add(new int[] {a, cost});
+        if (cost < minCost) { minCost = cost; minNode = a; }
+        costSum += cost;
+      }
+      System.out.println(costSum - prim(minNode));
+    }
+    scanner.close();
+  }
+}
