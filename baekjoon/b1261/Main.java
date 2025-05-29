2efef827d603d127283c23b3732ac2045c51350e
Lee, Seungjin <quack337@gmail.com>
2025-05-30 08:32:26 +0900

    b1261

--- baekjoon/b1261/Main.java
+++ baekjoon/b1261/Main.java
@@ -1,54 +1,37 @@
-package baekjoon.b1261;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+package baekjoon.b1916;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static class Node implements Comparable<Node> {
-        int r, c, distance;
+  static int ROW, COL;
+  static char[][] A;
 
-        public Node(int r, int c, int distance) {
-            this.r = r;
-            this.c = c;
-            this.distance = distance;
-        }
-
-        @Override
-        public int compareTo(Node n) {
-            return distance - n.distance;
-        }
+  static int dijkstra() {
+    var visited = new boolean[ROW][COL];
+    var queue = new PriorityQueue<int[]>((a, b) -> a[2] - b[2]);
+    queue.add(new int[] {0, 0, 0});
+    while (queue.size() > 0) {
+      int[] u = queue.remove();
+      int r = u[0], c = u[1], distance = u[2];
+      if (visited[r][c]) continue;
+      visited[r][c] = true;
+      if (r == ROW-1 && c == COL-1) return distance;
+      if (r > 0) queue.add(new int[] {r-1, c, distance + A[r-1][c] - '0'});
+      if (c > 0) queue.add(new int[] {r, c-1, distance + A[r][c-1] - '0'});
+      if (r < ROW-1) queue.add(new int[] {r+1, c, distance + A[r+1][c] - '0'});
+      if (c < COL-1) queue.add(new int[] {r, c+1, distance + A[r][c+1] - '0'});
     }
+    return -1;
+  }
 
-    static int dijstra(char[][] A, int N, int M) {
-        boolean[][] visited = new boolean[N][M];
-        PriorityQueue<Node> queue = new PriorityQueue<>();
-        queue.add(new Node(0, 0, (A[0][0] - '0')));
-        while (queue.size() > 0) {
-            Node node = queue.remove();
-            int r = node.r, c = node.c, distance = node.distance;
-            if (r < 0 || r >= N) continue;
-            if (c < 0 || c >= M) continue;
-            if (r == N-1 && c == M-1) return distance;
-            if (visited[r][c]) continue;
-            visited[r][c] = true;
-            if (r > 0) queue.add(new Node(r-1, c, distance + (A[r-1][c] - '0')));
-            if (c > 0) queue.add(new Node(r, c-1, distance + (A[r][c-1] - '0')));
-            if (r < N-1) queue.add(new Node(r+1, c, distance + (A[r+1][c] - '0')));
-            if (c < M-1) queue.add(new Node(r, c+1, distance + (A[r][c+1] - '0')));
-        }
-        return -1;
-    }
-
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
-        int M = Integer.parseInt(tokenizer.nextToken());
-        int N = Integer.parseInt(tokenizer.nextToken());
-        char[][] A = new char[N][];
-        for (int i = 0; i < N; ++i)
-            A[i] = reader.readLine().toCharArray();
-        System.out.println(dijstra(A, N, M));
-    }
+  public static void main(String[] args) throws IOException {
+    var scanner = new Scanner(new BufferedInputStream(System.in));
+    COL = scanner.nextInt();
+    ROW = scanner.nextInt();
+    A = new char[ROW][];
+    for (int r = 0; r < ROW; ++r)
+      A[r] = scanner.next().toCharArray();
+    scanner.close();
+    System.out.println(dijkstra());
+  }
 }
