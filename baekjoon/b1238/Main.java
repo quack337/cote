8f74d46f8332340a9888f71bbbb59d17d7fb1c70
Lee, Seungjin <quack337@gmail.com>
2025-05-30 07:29:34 +0900

    b1238

--- baekjoon/b1238/Main.java
+++ baekjoon/b1238/Main.java
@@ -1,49 +1,58 @@
 package baekjoon.b1238;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+// 단방향 간선, 모든지점 왕복 최대거리
+// 갈 때 간선 edges1, 올 때 간선 edges2
+// 모든지점 최단거리 찾기
+import java.io.*;
+import java.util.*;
 
 public class Main {
+  static int N, E, START;
 
-    static int add(int a, int b) {
-        if (a == Integer.MAX_VALUE || b == Integer.MAX_VALUE)
-            return Integer.MAX_VALUE;
-        return a + b;
+  static int[] dijkstra(ArrayList<int[]>[] edges) {
+    var distances = new int[N+1];
+    Arrays.fill(distances, -1);
+    var queue = new PriorityQueue<int[]>((a, b) -> a[1] - b[1]);
+    queue.add(new int[] {START, 0});
+    while (queue.size() > 0) {
+      int[] u = queue.remove();
+      int node = u[0], distance = u[1];
+      if (distances[node] > -1) continue;
+      distances[node] = distance;
+      for (int[] edge : edges[node]) {
+        int neighbor = edge[0], cost = edge[1];
+        queue.add(new int[] {neighbor, distance + cost});
+      }
     }
+    return distances;
+  }
 
-    static void floydWarshall(int[][] A, int N) {
-        for (int k = 0; k < N; ++k)
-            for (int i = 0; i < N; ++i)
-                for (int j = 0; j < N; ++j)
-                    A[i][j] = Math.min(A[i][j], add(A[i][k], A[k][j]));
+  @SuppressWarnings("unchecked")
+  public static void main(String[] args) throws IOException {
+    var scanner = new Scanner(new BufferedInputStream(System.in));
+    N = scanner.nextInt();
+    E = scanner.nextInt();
+    START = scanner.nextInt();
+    ArrayList<int[]>[] edges1 = new ArrayList[N+1];
+    ArrayList<int[]>[] edges2 = new ArrayList[N+1];
+    for (int i = 1; i <= N; ++i) {
+      edges1[i] = new ArrayList<int[]>();
+      edges2[i] = new ArrayList<int[]>();
     }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
-        int N = Integer.parseInt(tokenizer.nextToken());
-        int M = Integer.parseInt(tokenizer.nextToken());
-        int X = Integer.parseInt(tokenizer.nextToken()) - 1;
-        int[][] A = new int[N][N];
-        for (int i = 0; i < N; ++i) {
-            Arrays.fill(A[i], Integer.MAX_VALUE);
-            A[i][i] = 0;
-        }
-        for (int i = 0; i < M; ++i) {
-            tokenizer = new StringTokenizer(reader.readLine());
-            int a = Integer.parseInt(tokenizer.nextToken()) - 1;
-            int b = Integer.parseInt(tokenizer.nextToken()) - 1;
-            int c = Integer.parseInt(tokenizer.nextToken());
-            A[a][b] = c;
-        }
-        floydWarshall(A, N);
-        int max = 0;
-        for (int i = 0; i < N; ++i) {
-            int travel = A[i][X] + A[X][i];
-            if (travel > max) max = travel;
-        }
-        System.out.println(max);
+    for (int i = 0; i < E; ++i) {
+      int a = scanner.nextInt();
+      int b = scanner.nextInt();
+      int cost = scanner.nextInt();
+      edges1[a].add(new int[] {b, cost});
+      edges2[b].add(new int[] {a, cost});
     }
+    scanner.close();
+    int[] distances1 = dijkstra(edges1);
+    int[] distances2 = dijkstra(edges2);
+    int max = 0;
+    for (int i = 1; i <= N; ++i) {
+      int distance = distances1[i] + distances2[i];
+      if (distance > max) max = distance;
+    }
+    System.out.println(max);
+  }
 }
