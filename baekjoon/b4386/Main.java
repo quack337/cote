49176e0c84c6b18c791cdcd6588562aefed6a818
Lee, Seungjin <quack337@gmail.com>
2025-05-31 10:53:58 +0900

    b4386

--- baekjoon/b4386/Main.java
+++ baekjoon/b4386/Main.java
@@ -3,12 +3,12 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-  static int V, E;
+  static int N, E;
   static ArrayList<float[]>[] links;
 
   static float prim(int start) {
     float costSum = 0;
-    var visited = new boolean[V+1];
+    var visited = new boolean[N+1];
     var queue = new PriorityQueue<float[]>((a, b) -> Float.compare(a[1], b[1]));
     queue.add(new float[] {start, 0});
     while (queue.size() > 0) {
@@ -26,18 +26,18 @@ public class Main {
   @SuppressWarnings("unchecked")
   public static void main(String[] args) throws IOException {
     var scanner = new Scanner(new BufferedInputStream(System.in));
-    V = scanner.nextInt();
-    var stars = new float[V][2];
-    for (int i = 0; i < V; ++i) {
+    N = scanner.nextInt();
+    var stars = new float[N][2];
+    for (int i = 0; i < N; ++i) {
       stars[i][0] = scanner.nextFloat();
       stars[i][1] = scanner.nextFloat();
     }
-    links = new ArrayList[V+1];
-    for (int a = 0; a < V; ++a)
+    links = new ArrayList[N+1];
+    for (int a = 0; a < N; ++a)
       links[a] = new ArrayList<>();
     float min = Float.MAX_VALUE; int minNode = 0;
-    for (int a = 0; a < V-1; ++a)
-      for (int b = a+1; b < V; ++b) {
+    for (int a = 0; a < N-1; ++a)
+      for (int b = a+1; b < N; ++b) {
         float dx = stars[a][0] - stars[b][0];
         float dy = stars[a][1] - stars[b][1];
         float cost = (float)Math.sqrt(dx*dx + dy*dy);
