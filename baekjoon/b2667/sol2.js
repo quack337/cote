e9fd538740eb506f3d8f8dbec9bd647f9e51e59a
Lee, Seungjin <quack337@gmail.com>
2025-06-11 13:34:41 +0900

    b2667

--- baekjoon/b2667/sol2.js
+++ baekjoon/b2667/sol2.js
@@ -2,32 +2,38 @@
 let fs = require('fs');
 let input = fs.readFileSync(0).toString().split(/[ \n\r]+/), input_idx = 0;
 let getStr = () => input[input_idx++], getInt = () => parseInt(getStr());
-let N = getInt();
+let ROW = getInt(), COL = ROW;
 let A = [];
-for (let i = 0; i < N; ++i)
-  A[i] = getStr().split('');
-let NOT_VISITED = '1', VISITED = 'x';
-let answer = [];
-for (let r = 0; r < N; ++r)
-  for (let c  = 0; c < N; ++c)
-    if (A[r][c] == NOT_VISITED)
-      answer.push(DFS(r, c));
-answer.sort((a, b) => a - b);
-answer.unshift(answer.length);
+for (let r = 0; r < ROW; ++r)
+  A[r] = getStr().split('');
+let 집 = '1';
+let sizes = 연결그래프크기();
+sizes.sort((a, b) => a - b);
+let answer = [sizes.length, ...sizes];
 console.log(answer.join('\n'));
 
+function 연결그래프크기() {
+  let sizes = [];
+  visited = Array(ROW).fill().map(() => []);
+  for (let r = 0; r < ROW; ++r)
+    for (let c  = 0; c < COL; ++c)
+      if (!visited[r][c] && A[r][c] == 집)
+        sizes.push(DFS(r, c));
+  return sizes;
+}
+
 function DFS(rStart, cStart) {
-  let stack = [], count = 0;
+  let stack = [], size = 0;
   stack.push([rStart, cStart]);
   while (stack.length > 0) {
     let [r, c] = stack.pop();
-    if (A[r][c] != NOT_VISITED) continue;
-    A[r][c] = VISITED;
-    ++count;
+    if (visited[r][c] || A[r][c] != 집) continue;
+    visited[r][c] = true;
+    ++size;
     if (r > 0) stack.push([r-1, c]);
     if (c > 0) stack.push([r, c-1]);
-    if (r < N-1) stack.push([r+1, c]);
-    if (c < N-1) stack.push([r, c+1]);
+    if (r < ROW-1) stack.push([r+1, c]);
+    if (c < COL-1) stack.push([r, c+1]);
   }
-  return count;
+  return size;
 }
