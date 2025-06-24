43fa565075cc9bbfc6dc6884ee0641ccabb6bc70
Lee, Seungjin <quack337@gmail.com>
2025-06-24 09:30:59 +0900

    b1937

---
+++ baekjoon/b1937/Main1.java
@@ -0,0 +1,34 @@
+package baekjoon.b1937;
+import java.io.*;
+import java.util.*;
+
+// 1단계: DFS 테스트
+public class Main1 {
+  static int ROW, COL;
+  static int[][] A;
+
+  static int DFS(int r, int c) {
+    int d = 0;
+    if (r > 0 && A[r-1][c] > A[r][c]) d = Math.max(d, DFS(r-1, c));
+    if (c > 0 && A[r][c-1] > A[r][c]) d = Math.max(d, DFS(r, c-1));
+    if (r < ROW-1 && A[r+1][c] > A[r][c]) d = Math.max(d, DFS(r+1, c));
+    if (c < COL-1 && A[r][c+1] > A[r][c]) d = Math.max(d, DFS(r, c+1));
+    return d + 1;
+  }
+
+  public static void main(String[] args) throws IOException {
+    var scanner = new Scanner(new BufferedInputStream(System.in));
+    ROW = scanner.nextInt();
+    COL = ROW;
+    A = new int[ROW][COL];
+    for (int r = 0; r < ROW; ++r)
+      for (int c = 0; c < COL; ++c)
+        A[r][c] = scanner.nextInt();
+    scanner.close();
+    for (int r = 0; r < ROW; ++r) {
+      for (int c = 0; c < COL; ++c)
+        System.out.print(DFS(r, c) + " ");
+      System.out.println();
+    }
+  }
+}
