ac234d3fbd48fd8fe9a505f9d9fee056e4d78e6b
Lee, Seungjin <quack337@gmail.com>
2025-06-24 12:18:44 +0900

    b17471

--- baekjoon/b17471/Main.java
+++ baekjoon/b17471/Main.java
@@ -3,74 +3,60 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-  static int N, SUM, answer;
-  static int[] A;
+  static int N, 인구합계, answer;
+  static int[] 인구;
   static List<Integer>[] neighbors;
-  static ArrayDeque<Integer> selected, notSelected;
+  static Set<Integer> 구역1, 구역2;
 
-  static boolean 연결그래프(ArrayDeque<Integer> nodes) {
+  static boolean 연결그래프(Set<Integer> nodes) {
     var stack = new ArrayDeque<Integer>();
     var visited = new boolean[N+1];
-    stack.push(nodes.getFirst());
+    stack.push(nodes.iterator().next());
+    int count = 0;
     while (stack.size() > 0) {
       int node = stack.pop();
       if (visited[node]) continue;
       visited[node] = true;
+      ++count;
       for (int neighbor : neighbors[node])
-        stack.push(neighbor);
+        if (nodes.contains(neighbor))
+          stack.push(neighbor);
     }
-    for (int node : nodes)
-      if (visited[node] == false) 
-        return false;
-    return true;
+    return count == nodes.size();
   }
 
-  static int sum() {
-    int r = 0;
-    for (int node : selected)
-      r += A[node];
-    return r;
-  }
-
-  static void DFS(int index) {
+  static void subset(int index) {
     if (index > N) {
-      if (selected.size() == 0 || !연결그래프(selected)) return;
-      if (notSelected.size() > 0 && !연결그래프(notSelected)) return;
-      answer = Math.min(answer,  Math.abs(SUM - sum() * 2));
+      if (구역1.size() > 0 && 구역2.size() > 0 && 연결그래프(구역1) && 연결그래프(구역2)) {
+        int 구역1인구 = 구역1.stream().reduce(0, (sum, i) -> sum + 인구[i]);
+        answer = Math.min(answer,  Math.abs(인구합계 - 구역1인구 * 2));
+      }
       return;
     }
-    selected.push(index);
-    DFS(index + 1);
-    selected.pop(); 
-    notSelected.push(index);
-    DFS(index + 1);
-    notSelected.pop();
+    구역1.add(index); subset(index + 1); 구역1.remove(index);
+    구역2.add(index); subset(index + 1); 구역2.remove(index);
   }
 
   @SuppressWarnings("unchecked")
   public static void main(String[] args) throws IOException {
     var scanner = new Scanner(new BufferedInputStream(System.in));
     N = scanner.nextInt();
-    A = new int[N+1];
-    SUM = 0;
-    for (int i = 1; i <= N; ++i) {
-      A[i] = scanner.nextInt();
-      SUM += A[i];
-    }
+    인구 = new int[N+1];
+    for (int i = 1; i <= N; ++i)
+      인구[i] = scanner.nextInt();
+    인구합계 = Arrays.stream(인구).sum();
     neighbors = new ArrayList[N+1];
     for (int a = 1; a <= N; ++a) {
       neighbors[a] = new ArrayList<>();
       int count = scanner.nextInt();
-      for (int j = 0; j < count; ++j) {
-        int b = scanner.nextInt();
-        neighbors[a].add(b);
-      }
+      for (int j = 0; j < count; ++j)
+        neighbors[a].add(scanner.nextInt());
     }
-    selected = new ArrayDeque<>();
-    notSelected = new ArrayDeque<>();
+    구역1 = new HashSet<>();
+    구역2 = new HashSet<>();
     scanner.close();
-    answer = Integer.MAX_VALUE;
-    DFS(1);
-    System.out.println(answer == Integer.MAX_VALUE ? -1 : answer);
+    answer = 999_999;
+    subset(1);
+    System.out.println(answer == 999_999 ? -1 : answer);
   }
 }
