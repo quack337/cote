8e3c7a3e022934045aeee270631f7f92b8065682
Lee, Seungjin <quack337@gmail.com>
2025-06-26 08:24:31 +0900

    b3109

---
+++ baekjoon/b3109/Main1.java
@@ -0,0 +1,40 @@
+package baekjoon.b3109;
+import java.io.*;
+import java.util.*;
+
+// 시간초과
+public class Main1 {
+
+  static int ROW, COL;
+  static char[][] A;
+  static boolean[][] visited;
+
+  static boolean DFS(int r, int c) {
+    if (visited[r][c] || A[r][c] == 'x') return false;
+    visited[r][c] = true;
+    if (c == COL-1) return true;
+    if (r > 0 && DFS(r-1, c)) return true;
+    if (r > 0 && DFS(r-1, c+1)) return true;
+    if (DFS(r, c+1)) return true;
+    if (r < ROW-1 && DFS(r+1, c+1)) return true;
+    if (r < ROW-1 && DFS(r+1, c)) return true;
+    visited[r][c] = false;
+    return false;
+  }
+
+  public static void main(String[] args) throws IOException {
+    var scanner = new Scanner(new BufferedInputStream(System.in));
+    ROW = scanner.nextInt();
+    COL = scanner.nextInt();
+    A = new char[ROW][];
+    for (int r = 0; r < ROW; ++r)
+      A[r] = scanner.next().toCharArray();
+    scanner.close();
+    visited = new boolean[ROW][COL];
+    int answer = 0;
+    for (int r = 0; r < ROW; ++r)
+      if (DFS(r, 0))
+        ++answer;
+    System.out.println(answer);
+  }
+}
---
+++ baekjoon/b3109/run.py
@@ -0,0 +1,80 @@
+import subprocess
+
+#command = "node sol.js"
+command = "java Main.java"
+run = 1
+
+def testFS(inputFile, outputString):
+  cmd = command + " < " + inputFile
+  result = subprocess.check_output(cmd, shell=True, text=True)
+  if result.strip() == outputString:
+    print(inputFile, True)
+  else: print(inputFile, False, result);
+
+def testSS(inputString, outputString):
+  global run
+  with open("data_temp", "w") as f:
+    f.write(inputString)
+  cmd = command + " < data_temp"
+  result = subprocess.check_output(cmd, shell=True, text=True)
+  tag = "input" + str(run)
+  run = run + 1
+  if result.strip() == outputString:
+    print(tag, True)
+  else:
+    print(tag, False, result)
+    exit()
+
+testFS("data1", "2")
+testFS("data2", "5")
+
+testSS("""2 2
+..
+..
+""", "2")
+
+testSS("""3 3
+...
+...
+...
+""", "3")
+
+testSS("""3 3
+.x.
+...
+...
+""", "2")
+
+testSS("""3 3
+.x.
+.x.
+...
+""", "1")
+
+testSS("""3 3
+.x.
+.x.
+.x.
+""", "0")
+
+testSS("""4 4
+....
+....
+....
+....
+""", "4")
+
+testSS("""4 4
+.x..
+..x.
+.x..
+..x.
+""", "2")
+
+testSS("""5 5
+.x...
+.x.x.
+.x.x.
+.x.x.
+...x.
+""", "1")
