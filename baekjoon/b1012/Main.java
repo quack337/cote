a27650e5c9fd82c07bf8c6ff6654b8b4aff83f73
Lee, Seungjin <quack337@gmail.com>
2025-06-27 10:04:56 +0900

    b1012

--- baekjoon/b1012/Main.java
+++ baekjoon/b1012/Main.java
@@ -1,45 +1,45 @@
 package baekjoon.b1012;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+  static int ROW, COL, 배추 = 1;
+  static boolean[][] visited;
+  static int[][] A;
 
-    static void DFS(int[][] A, int r, int c) {
-        if (r < 0 || r >= A.length) return;
-        if (c < 0 || c >= A[0].length) return;
-        if (A[r][c] != 1) return;
-        A[r][c] = 0;
-        DFS(A, r-1, c);
-        DFS(A, r+1, c);
-        DFS(A, r, c-1);
-        DFS(A, r, c+1);
-    }
+  static void DFS(int r, int c) {
+    if (visited[r][c] || A[r][c] != 배추) return;
+    visited[r][c] = true;
+    if (r > 0) DFS(r-1, c);
+    if (c > 0) DFS(r, c-1);
+    if (r < ROW-1) DFS(r+1, c);
+    if (c < COL-1)DFS(r, c+1);
+  }
 
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(reader.readLine());
-        for (int t = 0; t < T; ++t) {
-            StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
-            int M = Integer.parseInt(tokenizer.nextToken());
-            int N = Integer.parseInt(tokenizer.nextToken());
-            int K = Integer.parseInt(tokenizer.nextToken());
-            int[][] A = new int[M][N];
-            for (int k = 0; k < K; ++k) {
-                tokenizer = new StringTokenizer(reader.readLine());
-                int r = Integer.parseInt(tokenizer.nextToken());
-                int c = Integer.parseInt(tokenizer.nextToken());
-                A[r][c] = 1;
-            }
-            int count = 0;
-            for (int r = 0; r < A.length; ++r)
-                for (int c = 0; c < A[0].length; ++c)
-                    if (A[r][c] == 1) {
-                        ++count;
-                        DFS(A, r, c);
-                    }
-            System.out.println(count);
-        }
+  public static void main(String[] args) throws NumberFormatException, IOException {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    int T = Integer.parseInt(reader.readLine());
+    for (int t = 0; t < T; ++t) {
+      StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+      ROW = Integer.parseInt(tokenizer.nextToken());
+      COL = Integer.parseInt(tokenizer.nextToken());
+      int K = Integer.parseInt(tokenizer.nextToken());
+      A = new int[ROW][COL];
+      for (int k = 0; k < K; ++k) {
+        tokenizer = new StringTokenizer(reader.readLine());
+        int r = Integer.parseInt(tokenizer.nextToken());
+        int c = Integer.parseInt(tokenizer.nextToken());
+        A[r][c] = 1;
+      }
+      int answer = 0;
+      visited = new boolean[ROW][COL];
+      for (int r = 0; r < ROW; ++r)
+        for (int c = 0; c < COL; ++c)
+          if (!visited[r][c] && A[r][c] == 배추) { // 방문하지 않은 배추칸
+            ++answer;
+            DFS(r, c);
+          }
+      System.out.println(answer);
     }
+  }
 }
--- baekjoon/b1012/sol1.js
+++ baekjoon/b1012/sol1.js
@@ -3,19 +3,20 @@ let fs = require('fs');
 let input = fs.readFileSync(0).toString().split(/[ \n\r]+/), input_idx = 0;
 let getStr = () => input[input_idx++], getInt = () => parseInt(getStr());
 let T = getInt();
-let 빈칸 = 0, 배추 = 1, VISITED = -1;
-let A, ROW, COL, K;
+let 배추 = 1;
+let A, ROW, COL, K, visited;
 for (let t = 0; t < T; ++t) {
   COL = getInt(); ROW = getInt(); K = getInt();
-  A = Array(ROW).fill().map(() => Array(COL).fill(빈칸));
+  A = Array(ROW).fill().map(() => Array(COL).fill(0));
   for (let i = 0; i < K; ++i) {
     let c = getInt(), r = getInt();
     A[r][c] = 배추;
   }
   let answer = 0;
+  visited = Array(ROW).fill().map(() => Array(COL).fill(false));
   for (let r = 0; r < ROW; ++r)
     for (let c  = 0; c < COL; ++c)
-      if (A[r][c] == 배추) {
+      if (!visited[r][c] && A[r][c] == 배추) {
         ++answer;
         DFS(r, c);
       }
@@ -23,8 +24,8 @@ for (let t = 0; t < T; ++t) {
 }
 
 function DFS(r, c) {
-  if (A[r][c] != 배추) return 0;
-  A[r][c] = VISITED;
+  if (visited[r][c] || A[r][c] != 배추) return;
+  visited[r][c] = true;
   if (r > 0) DFS(r-1, c);
   if (c > 0) DFS(r, c-1);
   if (r < ROW-1) DFS(r+1, c);
