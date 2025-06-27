14d3cbedd62aacd1d3b916e8f4c4838abae50420
Lee, Seungjin <quack337@gmail.com>
2025-06-27 09:26:37 +0900

    b11724

--- baekjoon/b11724/Main.java
+++ baekjoon/b11724/Main.java
@@ -1,44 +1,41 @@
 package baekjoon.b11724;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static ArrayList<Integer>[] 이웃;
-    static boolean[] visited;
+  static ArrayList<Integer>[] neighbors;
+  static boolean[] visited;
 
-    static void DFS(int i) {
-        visited[i] = true;
-        for (int c : 이웃[i])
-            if (visited[c] == false)
-                DFS(c);
-    }
+  static void DFS(int node) {
+    if (visited[node]) return;
+    visited[node] = true;
+    for (int neighbor : neighbors[node])
+      DFS(neighbor);
+  }
 
-    @SuppressWarnings("unchecked")
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
-        int N = Integer.parseInt(tokenizer.nextToken());
-        int M = Integer.parseInt(tokenizer.nextToken());
-        이웃 = new ArrayList[N];
-        for (int i = 0; i < N; ++i)
-            이웃[i] = new ArrayList<Integer>();
-        for (int i = 0; i < M; ++i) {
-            tokenizer = new StringTokenizer(reader.readLine());
-            int a = Integer.parseInt(tokenizer.nextToken()) - 1;
-            int b = Integer.parseInt(tokenizer.nextToken()) - 1;
-            이웃[a].add(b);
-            이웃[b].add(a);
-        }
-        visited = new boolean[N];
-        int count = 0;
-        for (int i = 0; i < N; ++i)
-            if (visited[i] == false) {
-                ++count;
-                DFS(i);
-            }
-        System.out.println(count);
+  @SuppressWarnings("unchecked")
+  public static void main(String[] args) throws IOException {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+    int N = Integer.parseInt(tokenizer.nextToken());
+    int M = Integer.parseInt(tokenizer.nextToken());
+    neighbors = new ArrayList[N+1];
+    for (int i = 1; i <= N; ++i)
+      neighbors[i] = new ArrayList<Integer>();
+    for (int i = 0; i < M; ++i) {
+      tokenizer = new StringTokenizer(reader.readLine());
+      int a = Integer.parseInt(tokenizer.nextToken());
+      int b = Integer.parseInt(tokenizer.nextToken());
+      neighbors[a].add(b);
+      neighbors[b].add(a);
     }
+    visited = new boolean[N+1];
+    int answer = 0;
+    for (int i = 1; i <= N; ++i)
+      if (visited[i] == false) {
+        ++answer;
+        DFS(i);
+      }
+    System.out.println(answer);
+  }
 }
