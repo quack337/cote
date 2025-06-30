693f1af570fdf7ba0b92098ce593dab12c45fbe3
Lee, Seungjin <quack337@gmail.com>
2025-06-30 13:16:51 +0900

    b15656

---
+++ baekjoon/b15656/Main.java
@@ -0,0 +1,37 @@
+package baekjoon.b15656;
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  static int N, M;
+  static int[] A;
+  static List<Integer> selected = new ArrayList<>();
+  static Writer wr = new BufferedWriter(new OutputStreamWriter(System.out));
+
+  static void DFS() throws IOException {
+    if (selected.size() == M) {
+      for (int i : selected)
+        wr.write(A[i] + " ");
+      wr.write("\n");
+      return;
+    }
+    for (int i = 0; i < N; ++i) {
+      selected.add(i);
+      DFS();
+      selected.remove(selected.size() - 1);
+    }
+  }
+
+  public static void main(String[] args) throws NumberFormatException, IOException {
+    var tk = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+    tk.nextToken(); N = (int)tk.nval;
+    tk.nextToken(); M = (int)tk.nval;
+    A = new int[N];
+    for (int i = 0; i < N; ++i) {
+      tk.nextToken(); A[i] = (int)tk.nval;
+    }
+    Arrays.sort(A);
+    DFS();
+    wr.close();
+  }
+}
