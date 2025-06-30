8c00b35d62dbb69b9b0342ef99f2a16d6ed8982a
Lee, Seungjin <quack337@gmail.com>
2025-06-30 14:04:22 +0900

    b15652

--- baekjoon/b15652/Main.java
+++ baekjoon/b15652/Main.java
@@ -5,28 +5,27 @@ import java.util.*;
 public class Main {
   static int N, M;
   static List<Integer> selected = new ArrayList<>();
+  static Writer wr = new BufferedWriter(new OutputStreamWriter(System.out));
 
-  static void DFS(Writer writer) throws IOException {
+  static void DFS(int from) throws IOException {
     if (selected.size() == M) {
-      for (int i : selected) writer.write(i + " ");
-      writer.write("\n");
+      for (int i : selected)
+        wr.write(i + " ");
+      wr.write("\n");
       return;
     }
-    int start = selected.size() == 0 ? 1 : selected.get(selected.size() - 1);
-    for (int i = start; i <= N; ++i) {
+    for (int i = from; i <= N; ++i) {
       selected.add(i);
-      DFS(writer);
+      DFS(i);
       selected.remove(selected.size() - 1);
     }
   }
 
   public static void main(String[] args) throws NumberFormatException, IOException {
-    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-    StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
-    N = Integer.parseInt(tokenizer.nextToken());
-    M = Integer.parseInt(tokenizer.nextToken());
-    DFS(writer);
-    writer.close();
+    var tk = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+    tk.nextToken(); N = (int)tk.nval;
+    tk.nextToken(); M = (int)tk.nval;
+    DFS(1);
+    wr.close();
   }
 }
