87d83ba6b2f41703993f46928c0b632f66bb1bc0
Lee, Seungjin <quack337@gmail.com>
2025-06-30 13:57:57 +0900

    b15650

--- baekjoon/b15650/Main.java
+++ baekjoon/b15650/Main.java
@@ -7,16 +7,15 @@ public class Main {
   static List<Integer> selected = new ArrayList<>();
   static Writer wr = new BufferedWriter(new OutputStreamWriter(System.out));
 
-  static void DFS() throws IOException {
+  static void DFS(int start) throws IOException {
     if (selected.size() == M) {
       for (int i : selected) wr.write(i + " ");
       wr.write("\n");
       return;
     }
-    int start = selected.size() == 0 ? 1 : selected.get(selected.size() - 1) + 1;
     for (int i = start; i <= N; ++i) {
       selected.add(i);
-      DFS();
+      DFS(i + 1);
       selected.remove(selected.size() - 1);
     }
   }
@@ -25,7 +24,7 @@ public class Main {
     var tk = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
     tk.nextToken(); N = (int)tk.nval;
     tk.nextToken(); M = (int)tk.nval;
-    DFS();
+    DFS(1);
     wr.close();
   }
 }
