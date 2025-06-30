3585297bd0599843482a2326c1281baca17abf9b
Lee, Seungjin <quack337@gmail.com>
2025-06-30 12:22:58 +0900

    b14889

--- baekjoon/b14889/Main.java
+++ baekjoon/b14889/Main.java
@@ -1,43 +1,45 @@
 package baekjoon.b14889;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+  static int N, ROW, COL, answer = Integer.MAX_VALUE;
+  static int[][] A;
+  static Set<Integer> selected = new HashSet<>();
 
-    static int[][] 능력치;
+  static int skill(Set<Integer> list) {
+    int val = 0;
+    for (int a : list)
+      for (int b : list)
+        if (a != b) val += A[a][b];
+    return val;
+  }
 
-    static int 능력치합계(int[] 팀) {
-        int 합계 = 0;
-        for (int 선수1 : 팀)
-            for (int 선수2 : 팀)
-                합계 += 능력치[선수1][선수2];
-        return 합계;
+  static void DFS(int from, int to) {
+    if (selected.size() == N/2) {
+      var notSelected = new HashSet<Integer>();
+      for (int i = 0; i < N; ++i)
+        if (!selected.contains(i)) notSelected.add(i);
+      int temp = Math.abs(skill(selected) - skill(notSelected));
+      if (temp < answer) answer = temp;
+      return;
     }
-
-    static int 계산(int[] 팀1, int 인덱스1, int[] 팀2, int 인덱스2, int 선수) {
-        if (선수 < 0)
-            return Math.abs(능력치합계(팀1) - 능력치합계(팀2));
-        int 능력치차이1 = Integer.MAX_VALUE, 능력치차이2 = Integer.MAX_VALUE;
-        if (인덱스1 < 팀1.length) {
-            팀1[인덱스1] = 선수;
-            능력치차이1 = 계산(팀1, 인덱스1+1, 팀2, 인덱스2, 선수-1);
-        }
-        if (인덱스2 < 팀2.length) {
-            팀2[인덱스2] = 선수;
-            능력치차이2 = 계산(팀1, 인덱스1, 팀2, 인덱스2+1, 선수-1);
-        }
-        return Math.min(능력치차이1, 능력치차이2);
+    for (int i = from; i <= to; ++i) {
+      selected.add(i);
+      DFS(i + 1, to + 1);
+      selected.remove(i);
     }
+  }
 
-    public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            int N = scanner.nextInt();
-            능력치 = new int[N][];
-            for (int r = 0; r < N; ++r) {
-                능력치[r] = new int[N];
-                for (int c = 0; c < N; ++c)
-                    능력치[r][c] = scanner.nextInt();
-            }
-            System.out.println(계산(new int[N/2], 0, new int[N/2], 0, N - 1));
-        }
-    }
+  public static void main(String[] args) throws IOException {
+   var tk = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+    tk.nextToken(); N = ROW = COL = (int)tk.nval;
+    A = new int[ROW][COL];
+    for (int r = 0; r < ROW; ++r)
+      for (int c = 0; c < COL; ++c) {
+        tk.nextToken(); A[r][c] = (int)tk.nval;
+      }
+    DFS(0, N - N/2);
+    System.out.println(answer);
+  }
 }
