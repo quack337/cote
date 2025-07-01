19ee406708a78b23a457a106c3464f77e4459c4b
Lee, Seungjin <quack337@gmail.com>
2025-07-02 04:18:35 +0900

    b15650

--- baekjoon/b15650/Main.java
+++ baekjoon/b15650/Main.java
@@ -3,19 +3,19 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-  static int N, M;
+  static int N, R;
   static List<Integer> selected = new ArrayList<>();
   static Writer wr = new BufferedWriter(new OutputStreamWriter(System.out));
 
-  static void DFS(int start) throws IOException {
-    if (selected.size() == M) {
+  static void DFS(int from, int to) throws IOException {
+    if (selected.size() == R) {
       for (int i : selected) wr.write(i + " ");
       wr.write("\n");
       return;
     }
-    for (int i = start; i <= N; ++i) {
+    for (int i = from; i <= N; ++i) {
       selected.add(i);
-      DFS(i + 1);
+      DFS(i + 1, to + 1);
       selected.remove(selected.size() - 1);
     }
   }
@@ -23,8 +23,8 @@ public class Main {
   public static void main(String[] args) throws NumberFormatException, IOException {
     var tk = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
     tk.nextToken(); N = (int)tk.nval;
-    tk.nextToken(); M = (int)tk.nval;
-    DFS(1);
+    tk.nextToken(); R = (int)tk.nval;
+    DFS(1, N-R+1);
     wr.close();
   }
 }
