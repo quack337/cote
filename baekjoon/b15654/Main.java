dbd248116ed7e45ce454462db5d23fb57b35af1b
Lee, Seungjin <quack337@gmail.com>
2025-07-02 09:54:37 +0900

    b15654

--- baekjoon/b15654/Main.java
+++ baekjoon/b15654/Main.java
@@ -3,21 +3,21 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-  static int N, M;
+  static int N, R;
   static int[] A;
   static List<Integer> selected = new ArrayList<>();
   static Writer wr = new BufferedWriter(new OutputStreamWriter(System.out));
 
   static void DFS() throws IOException {
-    if (selected.size() == M) {
-      for (int i : selected)
-        wr.write(A[i] + " ");
+    if (selected.size() == R) {
+      for (int val : selected)
+        wr.write(val + " ");
       wr.write("\n");
       return;
     }
     for (int i = 0; i < N; ++i)
-      if (!selected.contains(i)) {
-        selected.add(i);
+      if (!selected.contains(A[i])) {
+        selected.add(A[i]);
         DFS();
         selected.remove(selected.size() - 1);
       }
@@ -26,7 +26,7 @@ public class Main {
   public static void main(String[] args) throws NumberFormatException, IOException {
     var tk = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
     tk.nextToken(); N = (int)tk.nval;
-    tk.nextToken(); M = (int)tk.nval;
+    tk.nextToken(); R = (int)tk.nval;
     A = new int[N];
     for (int i = 0; i < N; ++i) {
       tk.nextToken(); A[i] = (int)tk.nval;
