444527e5610c6b21d0434cfe8f4854e3c3de0263
Lee, Seungjin <quack337@gmail.com>
2025-07-02 10:08:54 +0900

    b15656

--- baekjoon/b15656/Main.java
+++ baekjoon/b15656/Main.java
@@ -3,20 +3,20 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-  static int N, M;
+  static int N, R;
   static int[] A;
   static List<Integer> selected = new ArrayList<>();
   static Writer wr = new BufferedWriter(new OutputStreamWriter(System.out));
 
   static void DFS() throws IOException {
-    if (selected.size() == M) {
-      for (int i : selected)
-        wr.write(A[i] + " ");
+    if (selected.size() == R) {
+      for (int val : selected)
+        wr.write(val + " ");
       wr.write("\n");
       return;
     }
     for (int i = 0; i < N; ++i) {
-      selected.add(i);
+      selected.add(A[i]);
       DFS();
       selected.remove(selected.size() - 1);
     }
@@ -25,7 +25,7 @@ public class Main {
   public static void main(String[] args) throws NumberFormatException, IOException {
     var tk = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
     tk.nextToken(); N = (int)tk.nval;
-    tk.nextToken(); M = (int)tk.nval;
+    tk.nextToken(); R = (int)tk.nval;
     A = new int[N];
     for (int i = 0; i < N; ++i) {
       tk.nextToken(); A[i] = (int)tk.nval;
---
+++ baekjoon/b15656/sol.js
@@ -0,0 +1,23 @@
+let fs = require('fs');
+let input = fs.readFileSync(0).toString().split(/[ \n\r]+/), input_idx = 0;
+let getStr = () => input[input_idx++], getInt = () => parseInt(getStr());
+let N = getInt(), R = getInt();
+let A = [];
+for (let i = 0; i < N; ++i)
+  A[i] = getInt();
+let selected = [], answer = [];
+A.sort((a, b) => a - b);
+DFS();
+console.log(answer.join('\n'));
+
+function DFS() {
+  if (selected.length == R) {
+    answer.push(selected.join(' '));
+    return;
+  }
+  for (let i = 0; i < N; ++i) {
+      selected.push(A[i]);
+      DFS();
+      selected.pop();
+    }
+}
