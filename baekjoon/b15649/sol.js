2ef5c63f8efe91c40212f905b10c00fffd8dd8fd
Lee, Seungjin <quack337@gmail.com>
2025-07-02 09:32:04 +0900

    b15649

--- baekjoon/b15649/sol.js
+++ baekjoon/b15649/sol.js
@@ -2,12 +2,13 @@ let fs = require('fs');
 let input = fs.readFileSync(0).toString().split(/[ \n\r]+/), input_idx = 0;
 let getStr = () => input[input_idx++], getInt = () => parseInt(getStr());
 let N = getInt(), R = getInt();
-let selected = [];
+let selected = [], answer = [];
 DFS();
+console.log(answer.join('\n'));
 
 function DFS() {
   if (selected.length == R) {
-    process.stdout.write(selected.join(' ') + '\n');
+    answer.push(selected.join(' '));
     return;
   }
   for (let i = 1; i <= N; ++i)
