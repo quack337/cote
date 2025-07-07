a35ba76255ba46a7301a6404338685539ebcd262
Lee, Seungjin <quack337@gmail.com>
2025-07-07 11:51:25 +0900

    b1038

---
+++ baekjoon/b1038/sol.js
@@ -0,0 +1,11 @@
+let N = +require('fs').readFileSync(0).toString().trim(),
+ S = [];
+ DFS(9,0);
+ S.sort((a,b) => a-b);
+ console.log(N+1 < S.length ? S[N+1] : -1);
+
+function DFS(d, s) {
+  S.push(s);
+  for (let i = d; i >= 0; --i)
+    DFS(i-1, s*10+i);
+}
