128e1b75728b0f45840a3da5e2b68b3448d88905
Lee, Seungjin <quack337@gmail.com>
2025-07-12 12:09:26 +0900

    b17255

--- baekjoon/b17255/sol.js
+++ baekjoon/b17255/sol.js
@@ -1,20 +1,16 @@
-let IN=require('fs').readFileSync(0).toString().trim();
-N=+IN, ANS=0, A=[], V=[]
-for (let ch of IN.split('')) {
-  let i=+ch;
-  if (!V[i]) { V[i]=1; A.push(i); }
-}
-DFS('');
-console.log(ANS);
+let IN=require('fs').readFileSync(0).toString().trim(),
+N=+IN, X=0, A=IN.split('').reduce((r,e)=>r.includes(e)?r:[...r,e],[])
+DFS('')
+console.log(X)
 
 function DFS(s) {
-  if (IN.indexOf(s)<0) return;
-  let val = +s;
-  if (val==N) { ++ANS; return; }
-  if (val > N || s.length>=IN.length) return;
+  if (IN.indexOf(s)<0) return
+  let val= +s
+  if (val==N) {++X; return;}
+  if (val>N || s.length>=IN.length) return
   for (let i of A) {
-    let s1 = i+s, s2 = s+i;
-    DFS(s1);
-    if (s1 != s2) DFS(s2);
+    let s1= i+s, s2= s+i
+    DFS(s1)
+    if (s1!=s2) DFS(s2)
   }
 }
