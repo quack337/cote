5853c4940059b3c8ae79c6d349de01d98b4c6c76
Lee, Seungjin <quack337@gmail.com>
2025-07-13 12:43:14 +0900

    b21772

--- baekjoon/b21772/sol.js
+++ baekjoon/b21772/sol.js
@@ -1,7 +1,8 @@
 let IN=require('fs').readFileSync(0).toString().split('\n'),
 DD=[[-1,0],[0,1],[1,0],[0,-1]],
 [RN,CN,T]=IN[0].split(' ').map(e=>+e),
-A=IN.slice(1).map(s=>s.split('')), G, X=0, PATH=[]
+A=IN.slice(1).map(s=>s.split('')), G, X=0,
+V=Array(RN).fill().map(_=>Array(CN).fill().map(_=>Array(T)))
 for (let r=0; r<RN && !G; ++r)
   for (let c=0; c<CN; ++c)
     if (A[r][c]=='G') G=[r,c]
@@ -10,9 +11,10 @@ console.log(X)
 
 function DFS(r,c,t,x) {
   if (x+t <= X) return
-  if (A[r][c]=='#') return
+  if (V[r][c][t] || A[r][c]=='#') return
   if (x > X) X = x;
   if (t == 0) return
+  V[r][c][t]=1
   for (let [dr, dc] of DD) {
     let r1=r+dr, c1=c+dc
     if (r1<0||c1<0||r1>=RN||c1>=CN) continue
@@ -23,4 +25,5 @@ function DFS(r,c,t,x) {
       A[r1][c1]='S'
     } else DFS(r1,c1,t-1,x)
   }
+  V[r][c][t]=0
 }
