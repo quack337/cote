5264bb2cc8fa3bea170aa2b4efbe925a915603fb
Lee, Seungjin <quack337@gmail.com>
2025-07-18 16:37:47 +0900

    b15270

--- baekjoon/b15270/sol.js
+++ baekjoon/b15270/sol.js
@@ -1,15 +1,15 @@
 nums=s=>s.split(' ').map(e=>+e)
-D=require('fs').readFileSync(0).toString().split('\n')
+D=(require('fs').readFileSync(0)+'').split('\n')
 let [N,M]=nums(D[0])
 A=D.slice(1,M+1).map(s=>nums(s))
 V=[]
 X=0
-dfs=(n,cnt)=>{
-  if(n==A.length) {if (cnt>X) X=cnt; return}
+BT=(n,x)=>{
+  if(n==A.length) {if(x>X) X=x; return}
   let [a,b]=A[n]
-  if (!V[a]&&!V[b]) {V[a]=V[b]=1; dfs(n+1,cnt+2); V[a]=V[b]=0}
-  dfs(n+1,cnt)
+  if(!V[a]&&!V[b]) {V[a]=V[b]=1; BT(n+1,x+2); V[a]=V[b]=0}
+  BT(n+1,x)
 }
 
-dfs(0,0)
-console.log(Math.min(X+1,N))
+BT(0,0)
+console.log(X<N?X+1:N)
