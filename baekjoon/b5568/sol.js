f4492dd78efa9070d9362c2c098e990e617dffed
Lee, Seungjin <quack337@gmail.com>
2025-07-24 07:34:06 +0900

    b5568

--- baekjoon/b5568/sol.js
+++ baekjoon/b5568/sol.js
@@ -1,29 +1,24 @@
 let fs = require('fs');
-let input = fs.readFileSync(0).toString().split(/[ \n\r]+/), input_idx = 0;
-let getStr = () => input[input_idx++], getInt = () => parseInt(getStr());
+let input = fs.readFileSync(0).toString().split(/[ \n\r]+/), idx = 0;
+let getInt = () => parseInt(input[idx++]);
 let N = getInt(), R = getInt();
-let A = []; // 입력으로 주어진 수 목록
-let S = []; // 지금까지 선택한 수 목록
-let V = []; // 인덱스 i 위치의 수를 선택했는지 표시하기 위한 배열. 중복 선택을 막기 위함.
-let answer = new Set(); // 수를 선택해서 만든 정수를 담을 Set 객체. 중복된 값이 자동으로 걸러진다
+let A = [];
+let V = [];
+let answer = new Set();
 for (let i = 0; i < N; ++i)
-  A[i] = getStr(); // 선택한 수들을 문자열 연결할 거라고 정수로 변환하지 않는다
-DFS(0);
-console.log(answer.size); // Set 객체 담긴 정수의 갯수를 출력한다
+  A[i] = getInt();
+DFS(0, 0);
+console.log(answer.size);
 
-function DFS() {
-  if (S.length == R) { // R개를 선택했다면
-    // 선택한 수들을 연결하여 문자열을 만들고, 정수로 변환한다
-    let n = parseInt(S.join(''));
-    answer.add(n);
+function DFS(n, x) { // n: 지금까지 선택한 갯수, x: 선택한 수들로 계산한 값
+  if (n == R) {
+    answer.add(x);
     return;
   }
   for (let i = 0; i < N; ++i)
-    if (!V[i]) { // 인덱스 i 위치의 값을 아직 선택하지 않았다면
-      V[i] = true; // i 위치의 값을 선택했다고 표시
-      S.push(A[i]); // 선택한 수 목록에 A[i] 추가
-      DFS();         // 재귀호출
-      V[i] = false; // 재귀호출에서 리턴하자 마자 A[i] 선택을 취소한다
-      S.pop();
+    if (!V[i]) {
+      V[i] = true;
+      DFS(n+1, x * (A[i]>9 ? 100 : 10) + A[i]);
+      V[i] = false;
     }
 }
