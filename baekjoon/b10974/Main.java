7fb82343068c8698984d9f92f23ea7b450d98e88
Lee, Seungjin <quack337@gmail.com>
2025-07-23 14:03:34 +0900

    b10974

--- baekjoon/b10974/Main.java
+++ baekjoon/b10974/Main.java
@@ -1,31 +1,36 @@
-package baekjoon.b10947;
+package baekjoon.b10974;
 import java.io.*;
 import java.util.*;
 
 public class Main {
   static int N, R;
-  static List<Integer> selected = new ArrayList<>();
-  static Writer wr = new BufferedWriter(new OutputStreamWriter(System.out));
+  static boolean[] V; // i 값을 선택했는지 표시하기 위한 배열. 중복 선택을 막기 위함.
+  static List<Integer> S = new ArrayList<>(); // 지금까지 선택한 수 목록
+  static StringBuilder bd = new StringBuilder(); // 출력할 문자열
 
   static void DFS() throws IOException {
-    if (selected.size() == R) {
-      for (int i : selected)
-        wr.write(i + " ");
-      wr.write("\n");
+    if (S.size() == R) { // R개를 선택했다면
+      // 선택한 수 목록을 StringBuilder에 추가한다
+      for (int i : S)
+        bd.append(i).append(' ');
+      bd.append('\n');
       return;
     }
     for (int i = 1; i <= N; ++i)
-      if (!selected.contains(i)) {
-        selected.add(i);
-        DFS();
-        selected.remove(selected.size() - 1);
+      if (!V[i]) { // i를 아직 선택하지 않았다면
+        V[i] = true; // i를 선택했다고 표시
+        S.add(i); // 선택한 수 목록에 i 추가
+        DFS(); // 재귀호출
+        V[i] = false; // 재귀호출에서 리턴하자 마자 i 선택을 취소한다
+        S.remove(S.size() - 1);
       }
   }
 
   public static void main(String[] args) throws NumberFormatException, IOException {
-    var tk = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
-    tk.nextToken(); N = R = (int)tk.nval;
+    var rd = new BufferedReader(new InputStreamReader(System.in));
+    N = R = Integer.parseInt(rd.readLine());
+    V = new boolean[N+1];
     DFS();
-    wr.close();
+    System.out.println(bd.toString());
   }
 }
--- baekjoon/b10974/sol.js
+++ baekjoon/b10974/sol.js
@@ -1,20 +1,22 @@
 let fs = require('fs');
-let input = fs.readFileSync(0).toString().split(/[ \n\r]+/), input_idx = 0;
-let getStr = () => input[input_idx++], getInt = () => parseInt(getStr());
+let input = fs.readFileSync(0).toString().split(/[ \n\r]+/), idx = 0;
+let getInt = () => parseInt(input[idx++]);
 let N = getInt(), R = N;
-let selected = [], answer = [];
-DFS();
+let V = []; // i 값을 선택했는지 표시하기 위한 배열. 중복 선택을 막기 위함.
+let answer = []; // 출력할 답
+DFS(0, '');
 console.log(answer.join('\n'));
 
-function DFS() {
-  if (selected.length == R) {
-    answer.push(selected.join(' '));
+function DFS(n, s) { // n: 지금까지 선택한 갯수, s: 지금까지 선택한 수들을 연결한 문자열
+  if (n==R) { // R개를 선택했다면
+    // 선택한 수 목록을 문자열로 만들어서 answer 배열에 추가한다
+    answer.push(s.trim());
     return;
   }
   for (let i = 1; i <= N; ++i)
-    if (!selected.includes(i)) {
-      selected.push(i);
-      DFS();
-      selected.pop();
+    if (!V[i]) { // i를 아직 선택하지 않았다면
+      V[i] = true; // i를 선택했다고 표시
+      DFS(n+1, s+' '+i); // 재귀호출
+      V[i] = false; // 재귀호출에서 리턴하자 마자 i 선택을 취소한다
     }
 }
