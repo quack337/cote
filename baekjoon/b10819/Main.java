0535af0ba8a47128db200ed3d4cc1e4ec48352cf
Lee, Seungjin <quack337@gmail.com>
2025-07-25 07:32:28 +0900

    b10819

--- baekjoon/b10819/Main.java
+++ baekjoon/b10819/Main.java
@@ -3,23 +3,26 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-  static int N, R, answer = 0;
-  static int[] A;
-  static List<Integer> selected = new ArrayList<>();
+  static int N, R, 답 = 0;
+  static int[] A; // 입력으로 주어진 수 목록
+  static boolean[] V;  // 인덱스 i 위치의 수를 선택했는지 표시하기 위한 배열. 중복 선택을 막기 위함
+  static List<Integer> S = new ArrayList<>();  // 지금까지 선택한 선택한 숫자 목록
 
   static void DFS() throws IOException {
-      if (selected.size() == R) {
-      int sum = 0;
+      if (S.size() == R) { // N개를 선택했다면
+      int sum = 0; // 문제가 요구한 계산식을 계산한다
       for (int i = 0; i < N-1; ++i)
-        sum += Math.abs(A[selected.get(i)] - A[selected.get(i+1)]);
-      if (sum > answer) answer = sum;
+        sum += Math.abs(S.get(i) - S.get(i+1));
+      if (sum > 답) 답 = sum; // 최대값 찾기
       return;
     }
     for (int i = 0; i < N; ++i)
-    if (!selected.contains(i)) {
-        selected.add(i);
-        DFS();
-        selected.remove(selected.size() - 1);
+    if (!V[i]) { // 인덱스 i 위치의 수를 아직 선택하지 않았다면
+        V[i] = true;  // 인덱스 i 위치의 수를 선택했다고 표시
+        S.add(A[i]); // 인덱스 i 위치의 수를 선택 목록에 추가
+        DFS(); // 재귀호출
+        V[i] = false; // 재귀호출에러 리턴하자 마자 인덱스 i 위치의 수 선택을 취소한다
+        S.remove(S.size() - 1);
       }
   }
 
@@ -30,7 +33,8 @@ public class Main {
     for (int i = 0; i < N; ++i) {
       tk.nextToken(); A[i] = (int)tk.nval;
     }
+    V = new boolean[N];
     DFS();
-    System.out.println(answer);
+    System.out.println(답);
   }
 }
--- baekjoon/b10819/sol.js
+++ baekjoon/b10819/sol.js
@@ -1,25 +1,29 @@
 let fs = require('fs');
-let input = fs.readFileSync(0).toString().split(/[ \n\r]+/), input_idx = 0;
-let getStr = () => input[input_idx++], getInt = () => parseInt(getStr());
-let N = getInt(), R = N;
-let A = [], selected = [], answer = 0;
+let input = fs.readFileSync(0).toString().split(/[ \n\r]+/), idx = 0;
+let getInt = () => parseInt(input[idx++]);
+let N = getInt(), A = [];
 for (let i = 0; i < N; ++i)
   A[i] = getInt();
+let S = []; // 지금까지 선택한 선택한 숫자 목록
+let V = []; // 인덱스 i 위치의 수를 선택했는지 표시하기 위한 배열. 중복 선택을 막기 위함
+let 답 = 0;
 DFS();
-console.log(answer);
+console.log(답);
 
 function DFS() {
-  if (selected.length == R) {
-    let sum = 0;
+  if (S.length == N) { // N개를 선택했다면
+    let sum = 0; // 문제가 요구한 계산식을 계산한다
     for (let i = 0; i < N-1; ++i)
-      sum += Math.abs(A[selected[i]] - A[selected[i+1]]);
-    if (sum > answer) answer = sum;
+      sum += Math.abs(S[i] - S[i+1]);
+    if (sum > 답) 답 = sum; // 최대값 찾기
     return;
   }
   for (let i = 0; i < N; ++i)
-    if (!selected.includes(i)) {
-      selected.push(i);
-      DFS();
-      selected.pop();
+    if (!V[i]) { // 인덱스 i 위치의 수를 아직 선택하지 않았다면
+      V[i] = true;  // 인덱스 i 위치의 수를 선택했다고 표시
+      S.push(A[i]); // 인덱스 i 위치의 수를 선택 목록에 추가
+      DFS(); // 재귀호출
+      V[i] = false; // 재귀호출에러 리턴하자 마자 인덱스 i 위치의 수 선택을 취소한다
+      S.pop();
     }
 }
