ffe258718fe5171132a639a0feae077cd6548ada
Lee, Seungjin <quack337@gmail.com>
2025-07-27 08:04:58 +0900

    ...

---
+++ baekjoon/runcc.mjs
@@ -0,0 +1,28 @@
+import fs from 'fs';
+import util from 'util';
+import child_prcess from 'child_process';
+let exec = util.promisify(child_prcess.exec);
+
+async function getStdout(cmd) {
+  let { stdout } = await exec(cmd);
+  return stdout
+}
+
+async function testCCS(cmd1, cmd2, inputString) {
+  fs.writeFileSync("data_temp", inputString);
+  let out1 = (await exec(cmd1 + " < data_temp")).stdout.trim();
+  let out2 = (await exec(cmd2 + " < data_temp")).stdout.trim()
+  let label = "run with " + inputString;
+  if (out1 == out2)
+    console.log(label, true);
+  else {
+    console.log(label, false);
+    process.exit(1);
+  }
+}
+
+let cmd1 = "node sol.js"
+let cmd2 = "node sol_short2.js"
+
+for (let n=1; n<=1000; ++n)
+  await testCCS(cmd1, cmd2, n.toString())
