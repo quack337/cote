184e3bf29a5e648c7da84ab3aaa366695336d92e
Lee, Seungjin <quack337@gmail.com>
2025-08-19 10:48:03 +0900

    ...

--- baekjoon/b1699/sol.js
+++ baekjoon/b1699/sol.js
@@ -1,11 +1,9 @@
-N=+(require('fs').readFileSync(0)+'')
-M=Array(N+1).fill(-1)
-BT=n=>{
-  if (M[n]>-1) return M[n]
-  if (!n) return M[n]=0
-  let to=Math.sqrt(n), r=Infinity
-  for (let i=1; i<=to; ++i)
-    r = Math.min(r, BT(n-i*i)+1)
-  return M[n]=r
+N=+require('fs').readFileSync(0)
+a=1
+b=2
+for(i=3; i<=N; ++i) {
+  c = (a + b) % 15746
+  b = c
+  a = b
 }
-console.log(BT(N))
+console.log(c)
