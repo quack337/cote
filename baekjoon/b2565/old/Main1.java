b7398bf9e68adb68df4c8365bd9c2430f942292d
Lee, Seungjin <quack337@gmail.com>
2025-08-21 10:50:27 +0900

    ...

--- baekjoon/b2565/old/Main1.java
+++ baekjoon/b2565/old/Main1.java
@@ -1,30 +1,24 @@
-package baekjoon.b2565;
+package baekjoon.b2565.old;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main1 {
     static final int N = 501;
     static int[] A;
-    static int[][] DP;
 
     static int 오름차순_최대길이(int index, int previous) {
         if (index >= N) return 0;
-        if (DP[index][previous] > -1) return DP[index][previous];
         int r1 = 0, r2 = 0;
         if (A[index] > previous) r1 = 1 + 오름차순_최대길이(index + 1, A[index]);
         r2 = 오름차순_최대길이(index + 1, previous);
-        return DP[index][previous] = Math.max(r1, r2);
+        return Math.max(r1, r2);
     }
 
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         A = new int[N];
-        DP = new int[N][N];
-        for (int i = 0; i < N; ++i)
-            Arrays.fill(DP[i], -1);
         int 전기줄수 = Integer.parseInt(reader.readLine());
         for (int i = 0; i < 전기줄수; ++i) {
             StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
---
+++ baekjoon/b2565/sol1.js
@@ -0,0 +1,13 @@
+let IN = require('fs').readFileSync(0).toString().split('\n');
+let N = +IN.shift();
+let A = IN.map(s => s.split(' ').map(e=>+e));
+A.unshift([0, 0]);
+console.log(N - DFS(0));
+
+function DFS(prev) {
+  let r = 0, [s, e] = A[prev];
+  for (let i = 1; i <= N; ++i)
+    if (A[i][0] > s && A[i][1] > e)
+      r = Math.max(r, DFS(i) + 1);
+  return r;
+}
