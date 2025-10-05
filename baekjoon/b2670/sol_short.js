e3858a7bb6494bc3537b277e070421f5815005b5
Lee, Seungjin <quack337@gmail.com>
2025-10-05 16:32:33 +0900

    b2670

---
+++ baekjoon/b2670/sol_short.js
@@ -0,0 +1,8 @@
+let A=(require('fs').readFileSync(0)+'').split(/\s/).map(e=>+e);
+let N=A.shift();
+let P=A[0],Q=A[0];
+for (let i=1; i<N; ++i){
+ P=P>1?P*A[i]:A[i];
+ if(P>Q)Q=P;
+}
+console.log(Q.toFixed(3));
