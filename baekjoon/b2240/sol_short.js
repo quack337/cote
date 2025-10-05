4994f42f714410ccfa9e98b3040879c82ea43435
Lee, Seungjin <quack337@gmail.com>
2025-10-05 13:21:23 +0900

    b2240

---
+++ baekjoon/b2240/sol_short.js
@@ -0,0 +1,9 @@
+let A=(require('fs').readFileSync(0)+'').split(/\s/).map(e=>e-1);
+let [T,W]=A.splice(0,2);++T;++W;
+let M=Array(T).fill().map(_=>Array(W+1).fill().map(_=>[-1,-1]));
+console.log(Math.max(BT(0,W,0),BT(0,W-1,1)));
+function BT(t,w,i) {
+ if (t==T) return 0;
+ if (M[t][w][i]>-1) return M[t][w][i];
+ return M[t][w][i]=Math.max(BT(t+1,w,i),w?BT(t+1,w-1,i?0:1):0)+(A[t]==i?1:0);
+}
