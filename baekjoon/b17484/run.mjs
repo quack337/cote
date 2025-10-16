a734901121d0dd867728b500037727175ca6ba0a
Lee, Seungjin <quack337@gmail.com>
2025-10-16 10:21:12 +0900

    b17484

---
+++ baekjoon/b17484/run.mjs
@@ -0,0 +1,32 @@
+import fs from 'fs';
+import util from 'util';
+import child_prcess from 'child_process';
+let exec = util.promisify(child_prcess.exec);
+
+let command = "node sol.js"
+let inputCount = 1;
+
+async function testFS(inputFile, outputString) {
+  let cmd = command + " < " + inputFile
+  let { stdout, stderr } = await exec(cmd);
+  let label = inputFile != "data_temp" ? inputFile : "input" + inputCount++;
+  if (stdout.trim() == outputString)
+    console.log(label, true);
+  else {
+    console.log(label, false, stdout);
+    process.exit(1);
+  }
+}
+
+
+async function testSS(inputString, outputString) {
+  fs.writeFileSync("data_temp", inputString);
+  await testFS("data_temp", outputString);
+}
+
+await testFS('data1', '29');
+await testSS('1 1\n2', '2');
+await testSS('2 2\n1 2\n3 4', '4');
+await testSS('3 2\n1 2\n3 4\n5 6', '10');
+await testSS('3 2\n1 2\n3 4\n5 5', '9');
+await testSS('4 2\n1 2\n1 2\n2 1\n2 1', '4');
