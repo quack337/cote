35eab0862ed93b9986d51db317087d29f18952ba
Lee, Seungjin <quack337@gmail.com>
2025-11-06 09:43:56 +0900

    b2228

--- baekjoon/b2228/sol.js
+++ baekjoon/b2228/sol.js
@@ -1,11 +1,13 @@
-A=(require('fs').readFileSync(0)+'').split(/\s+/).map(e=>+e);
-[N,M]=A.shift();
-BT=(n,m)=>{
-  if(!m)return 0;
-  if(n==N)return -Infinity;
-  let x = -Infinity;
-  for(let a=n;a<N-2;++a){
-    let sum = A[a];
-    for (let b=a+2;b<N;sum+=A[b-1],++b){
-      let t = sum + 
+D=(require('fs').readFileSync(0)+'').split('\n');
+[N,M]=D.shift().split(' ').map(e=>+e);
+A=D.map(e=>+e);
+P=Array(N).fill().map(_=>Array(2).fill().map(_=>[]));
+BT=(n,m,p)=>{
+  if(m==0)return 0;
+  if(n>=N)return p&&m==1?0:-Infinity;
+  if(P[n][p][m]!=undefined)return P[n][p][m];
+  let a = p ? BT(n+1,m-1,0) : BT(n+1,m,0); // 선택 안하는 경우
+  let b = BT(n+1,m,1) + A[n]; // 선택하는 경우
+  return P[n][p][m] = a>b ? a : b;
 }
+console.log(BT(0,M,0));
