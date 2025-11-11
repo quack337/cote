232ac7f63b7bb1f27c8706715b36e0949808f067
Lee, Seungjin <quack337@gmail.com>
2025-11-11 09:50:42 +0900

    ...

--- baekjoon/b12920/sol5.js
+++ baekjoon/b12920/sol5.js
@@ -1,14 +1,28 @@
 A=(require('fs').readFileSync(0)+'').split('\n').map(s=>s.split(' ').map(e=>+e));
 [N,W]=A.shift();
-A.sort((a,b)=>b[1]/b[0]-a[1]/a[0]);
-for(let i=0,ww=W;i<N;++i){
-  let [w,,c]=A[i];
-  if(w*c<ww) ww = ww - w*c;
-  else { A[i][2]=ww/w|0; ww %= w; }
-} 
-console.log(N,W,A);
-return;
-
+map=[];
+for(let i=0;i<N;++i){
+  let [w,p,c] = A[i];
+  if(!map[w]) map[w]=[];
+  map[w].push(A[i]);
+}
+for(let i=0;i<map.length;++i)
+  if(map[i]){
+    map[i].sort((a,b)=>b[1]-a[1]);
+    for(let j=0,w=0;j<map[i].length; ++j){
+      w += map[i][j][0] * map[i][j][2];
+      if (w>=W){
+        let p=map[i][j][1];
+        for(let k=j+1;k<map[i].length;++k) map[i][k][2]=0;
+        for(let k=i*2;k<map.length;k+=i)
+          if(map[k])
+            for(let h=0;h<map[k].length;++h)
+              if(map[k][h][1]<=p) map[k][h][2] = 0;
+        break;  
+      }
+    }
+  }
+//console.log(N,W,A);return;
 P=Array(W+1).fill(0);
 Q=Array(W+1).fill(0);
 for(let i=0;i<N;++i){
