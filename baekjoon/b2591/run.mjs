e77d94c04e4f18d51fb86f356e39c122e9fbfac0
Lee, Seungjin <quack337@gmail.com>
2025-11-11 10:06:44 +0900

    b2591

---
+++ baekjoon/b2591/run.mjs
@@ -0,0 +1,32 @@
+import fs from 'fs';
+import util from 'util';
+import child_prcess from 'child_process';
+let exec = util.promisify(child_prcess.exec);
+
+let command = "node sol.js"
+let inputCount = 1;
+
+async function testFS(inputFile, outputString) {
+  let cmd = command + " < " + inputFile
+  let { stdout, stderr } = await exec(cmd);
+  let label = inputFile != "data_temp" ? inputFile : "input" + inputCount++;
+  if (stdout.trim() == outputString)
+    console.log(label, true);
+  else {
+    console.log(label, false, stdout);
+    process.exit(1);
+  }
+}
+
+
+async function testSS(inputString, outputString) {
+  fs.writeFileSync("data_temp", inputString);
+  await testFS("data_temp", outputString);
+}
+
+await testSS('27123', '6');
+await testSS('10', '1');
+await testSS('11', '2');
+await testSS('011', '0');
+await testSS('1010', '1');
+await testSS('1234', '5');
---
+++ baekjoon/b2591/sol.js
@@ -0,0 +1,10 @@
+A=(require('fs').readFileSync(0)+'').trim().split('').map(e=>+e);
+N=A.length;
+M=Array(N);
+BT=n=>{
+ if(n==N)return 1;
+ if(M[n]!=undefined)return M[n];
+ if(A[n]==0)return M[n]=0;
+ return M[n]=BT(n+1) + (n<N-1 && (A[n]<3||(A[n]==3&&A[n+1]<5)) ? BT(n+2) : 0);
+}
+console.log(BT(0))
