13cc1b3acd8bd8c7ca3810e97da33e2f53a4c96f
Lee, Seungjin <quack337@gmail.com>
2025-11-18 10:49:14 +0900

    ...

--- baekjoon/run.mjs
+++ baekjoon/run.mjs
@@ -6,11 +6,14 @@ let exec = util.promisify(child_prcess.exec);
 let command = "node sol.js"
 let inputCount = 1;
 
-async function testFS(inputFile, outputString) {
+async function testF(inputFile, output) {
   let cmd = command + " < " + inputFile
   let { stdout, stderr } = await exec(cmd);
   let label = inputFile != "data_temp" ? inputFile : "input" + inputCount++;
-  if (stdout.trim() == outputString)
+  let ok = (typeof output == "string") ?
+    (stdout.trim() == outputString) :
+    (Math.abs(Number(stdout.trim()) - output) <= 1e-9);
+  if (ok)
     console.log(label, true);
   else {
     console.log(label, false, stdout);
@@ -18,11 +21,11 @@ async function testFS(inputFile, outputString) {
   }
 }
 
-
-async function testSS(inputString, outputString) {
+async function testS(inputString, output) {
   fs.writeFileSync("data_temp", inputString);
-  await testFS("data_temp", outputString);
+  await testF("data_temp", output);
 }
 
-await testFS('data1', '5')
-await testSS('1', '1');
+await testF('data2', 1.0)
+
+await testS('2 1 1', 0.5);
