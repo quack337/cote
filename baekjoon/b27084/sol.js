1a2ceaa4af91ea2ced926b2315f9f6092c4f8233
Lee, Seungjin <quack337@gmail.com>
2025-11-24 17:40:59 +0900

    ...

--- baekjoon/b27084/sol.js
+++ baekjoon/b27084/sol.js
@@ -2,10 +2,18 @@ D=(require('fs').readFileSync(0)+'').split('\n').map(s=>s.split(' ').map(e=>+e))
 N=D[0][0];A=D[1];
 C=Array(N+1).fill(0);
 for(let i=0;i<N;++i)C[A[i]]++;
-E=1;F=0;
-for(let i=0;i<N;++i)
-  if(C[i]) {
-    E *= C[i]+1;
-    if(C[i]==1) ++F;
-  }
-console.log(E,F)
+F=0;H=0;J=1;
+for(let i=0;i<N;++i){
+  let c=C[i];
+  if(c>1) {H+=c; J*=(c+1)}
+  else if(c==1) ++F;
+
+}
+F=1<<F;
+H=1<<H;
+J=1<<J;
+G=H-J;
+K=F*G;
+E=1<<N;
+console.log(E,K)
+console.log("F:%d G:%d H:%d J:%d K:%d", F,G,H,J,K)
