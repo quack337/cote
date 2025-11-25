dfe3ce48628f350424a5d8007c58cda0b3848e7b
Lee, Seungjin <quack337@gmail.com>
2025-11-25 10:13:42 +0900

    ...

--- baekjoon/b27084/sol2.js
+++ baekjoon/b27084/sol2.js
@@ -5,15 +5,13 @@ for(let i=0;i<N;++i)C[A[i]]++;
 F=0;H=0;J=1;
 for(let i=0;i<N;++i){
   let c=C[i];
-  if(c>1) {H+=c; J*=(c+1)}
+  if(c>1) {H+=c; J*=c+1}
   else if(c==1) ++F;
 
 }
-F=1<<F;
-H=1<<H;
-J=1<<J;
-G=H-J;
-K=F*G;
-E=1<<N;
-console.log(E,K)
-console.log("F:%d G:%d H:%d J:%d K:%d", F,G,H,J,K)
+H=1<<H;J<<1;G=H-J;
+//console.log("H:%d J:%d G:%d", H, J, G);
+F=1<<F;K=F*G;
+//console.log("F:%d K:%d", F, K);
+E=(1<<N)-1;
+console.log(E-K)
