6a13c2f1a6d037b93eb25b69a77ff235b8d4221f
Seungjin Lee <quack337@gmail.com>
2025-11-28 10:19:07 +0900

    b13249

--- baekjoon/b13249/sol1.js
+++ baekjoon/b13249/sol1.js
@@ -1,22 +1,21 @@
-// 문제를 오해하고, T초 이후에 충돌하는 횟수를 계산함
 D=(require('fs').readFileSync(0)+'').split('\n');
 N=+D[0];
 A=D[1].split(' ').map(e=>+e);
 T=+D[2];
 B=[];
-CK=(a,b)=>a>=0 && b<N && C[a][0]+1==C[b][0] && C[a][1]==-1 && C[b][1]==1 ? 1 : 0;
-B2=_=>{
-  C=A.map((e,i)=>[e+B[i]*T,B[i]]).sort((a,b)=>a[0]-b[0]);
-  let a,b,x=0;
-  for(a=0;a<N;++a)if(C[a][1]>0)break;
-  for(b=N-1;b>=0;--b)if(C[b][1]<0)break;
-  if(a<b) console.log('A', a,b, C, b-a, CK(a-1,a), CK(b,b+1));
-  else if(a==b+1 && CK(b,a)) console.log('B', a,b,C, CK(b,a));
+F1=n=>{
+  if(n==N)return F2();
+  B[n]=-1;F1(n+1);
+  B[n]=+1;F1(n+1);
 }
-B1=n=>{
-  if(n==N) return B2();
-  B[n]=-1;B1(n+1);
-  B[n]=1;B1(n+1);
+F2=_=>{
+  for(let i=0;i<N;++i)
+    if(B[i]==1){
+      let s=A[i],e=A[i]+T;
+      for(let j=0;j<N;++j)
+        if(B[j]==-1 && s<A[j] && e>=A[j]-T) ++X;
+    }
 }
 X=0;
-B1(0);
+F1(0);
+console.log(X/(1<<N))
--- baekjoon/b13249/sol2.js
+++ baekjoon/b13249/sol2.js
@@ -1,38 +1,7 @@
 D=(require('fs').readFileSync(0)+'').split('\n');
-N=+D[0];
+N=+D[0];T=+D[2];
 A=D[1].split(' ').map(e=>+e);
-T=+D[2];
-CK=(B,a,b)=>a>=0 && b<N && B[a][0]+1==B[b][0] && B[a][1]==-1 && B[b][1]==1 ? 1 : 0;
-F3=(B,ck)=>{
-  B.sort((a,b)=>a[0]!=b[0] ? a[0]-b[0] : a[1]-b[1]);
-  let a,b,x=0;
-  for(a=0;a<N;++a)if(B[a][1]>0)break;
-  for(b=N-1;b>=0;--b)if(B[b][1]<0)break;
-  if(!ck)return a<b ? b-a : 0;
-  if(a<b)return b-a+CK(B,a-1,a)+CK(B,b,b+1);
-  else if(a==b+1) return CK(B,b,a);
-}
-F2=_=>{
-  let x0 = F3(B,0);
-  let C=B.map(b=>[b[0]+b[1]*T,b[1]]);
-  let x1 = F3(C,0);
-  if(x0-x1<0) {
-    console.log('error', x0-x1);
-    process.exit(0);
-  }
-  X += x0-x1;
-  if(x0-x1) {
-    console.log(B.map(e=>e.join()).join(' '),x0-x1, x0, x1);
-    //console.log(C.map(e=>e.join()).join(' '));
-  }
-}
-F1=n=>{
-  if(n==N) return F2();
-  B[n][0]=A[n];
-  B[n][1]=-1;F1(n+1);
-  B[n][1]=+1;F1(n+1);
-}
 X=0;
-B=A.map(e=>[]);
-F1(0)
-console.log(X,(1<<N))
+for(let i=0;i<N;++i)
+for(let j=0;j<N;++j)if(A[i]<A[j]&&A[i]+T>=A[j]-T)++X;
+console.log(X/4)
