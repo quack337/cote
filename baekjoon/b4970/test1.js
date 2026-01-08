459a1ae36dda23180d238ff12c12686e5f3c3fb8
Lee, Seungjin <quack337@gmail.com>
2026-01-08 11:30:15 +0900

    ...

--- baekjoon/b4970/test1.js
+++ baekjoon/b4970/test1.js
@@ -1,13 +1,14 @@
-S='(--R+(P*Q))';
-I=0;
+S='(--R+(P*Q))';I=0;
+FO=get();
+console.log(JSON.stringify(FO));
 
-function FO(){
+function get(){
   let c=S[I++];
   if(c=='0'||c=='1'||c=='2')return['N',+c];
   if(c=='P'||c=='Q'||c=='R')return['V',c];
-  if(c=='-')return['-',FO()];
+  if(c=='-')return['-',get()];
   if(c=='('){
-    let fo1=FO(),op=OP(),fo2=FO();
-    if(S[I++]!=')'){console.log('error');process.exit(0)}
+    let fo1=get(),op=S[I++],fo2=get();
+    if(S[I++]!=')' || (op!='+'&&op!='*')){console.log('error');process.exit(0)}
     return[op,fo1,fo2]}
 }
